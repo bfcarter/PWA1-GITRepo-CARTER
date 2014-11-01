626f15f00927a579aafc3c23a97db2c833e1a7bb
bfcarter <bfcarter@fullsail.edu>
2014-11-01 16:55:55 -0400

    Starting the duel

---
+++ PWA1:WK1:/goal1_assign_duel1/js/main.js
@@ -0,0 +1,31 @@
+/*
+ Name:Brittni Carter
+ Date:11/1/14
+ Class & Section:  PWA1-1411
+Assignment: Goal1: Assignment: Duel1
+ */
+
+(function(){
+
+    console.log("FIGHT");
+    //Player Name
+    var playerOneName = "Spiderman";
+    var playerTwoName = "Batman";
+
+    //player damage
+    var player1Damage = 20;
+    var player2Damage = 20;
+
+    //player health
+    var playerOneHealth = 100;
+    var playerTwoHealth = 100;
+
+    var round=1;
+
+    function fight() {
+        
+    };
+    function winnerCheck(){};
+    fight();
+
+});
