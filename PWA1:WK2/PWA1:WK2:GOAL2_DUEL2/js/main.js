327ef90b5d19f54e26819f4726518009e054681f
bfcarter <bfcarter@fullsail.edu>
2014-11-07 16:14:23 -0500

    duel2 adding comments

--- PWA1:WK2/PWA1:WK2:GOAL2_DUEL2/js/main.js
+++ PWA1:WK2/PWA1:WK2:GOAL2_DUEL2/js/main.js
@@ -1,8 +1,8 @@
 /*
  Name:Brittni Carter
- Date:11/1/14
+ Date:11/7/14
  Class & Section:  PWA1-1411
-Assignment: Goal1: Assignment: Duel1
+Assignment: Goal1: Assignment: Duel2
  */
 
 (function(){
@@ -27,7 +27,7 @@ var player2=["Paleman",20,100];
 
     function fight() {
 
-        alert(player1[0]+":"+player1[2]+"START"+player2[0]+":"+player2[2]);
+        alert(player1[0]+":"+player1[2]+"START"+player2[0]+":"+player2[2]); //start fight with alert with player names and 100 health
         for(var i=0;i < 10; i++){
 
             var minDamage1 = player1[1] * .5; //min round damage of player one with player ones health
@@ -35,8 +35,8 @@ var player2=["Paleman",20,100];
             var f1 = Math.floor(Math.random()*(player1[1]-minDamage1)+minDamage1); //randomizing the damage
             var f2 = Math.floor(Math.random()*(player2[1]-minDamage2)+minDamage2);
 
-            player1[2]-=f1;
-            player2[2]-=f2;
+            player1[2]-=f1; //player one damage
+            player2[2]-=f2;  //player two damage
 
             console.log(playerOneName+":"+player1[2]+""+playerTwoName+":"+player2[2]); //console log the health after rounds
             var result = winnerCheck();
