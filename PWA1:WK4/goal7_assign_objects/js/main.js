6a9facc49879d57b3990595352648f656c46d150
bfcarter <bfcarter@fullsail.edu>
2014-11-20 19:02:17 -0500

    adding comments to analyze objects

--- PWA1:WK4/goal7_assign_objects/js/main.js
+++ PWA1:WK4/goal7_assign_objects/js/main.js
@@ -3,5 +3,35 @@
  Date:11/19/14
  Class & Section:  PWA1-1411
  Assignment: objects.Person
+ main code
  */
 
+(function(){
+    //naming variables
+    var numOfPeople = 3; //number of people to be displayed
+    var people =[]; //selected people from names
+    var names = ["Brittni", "Esther", "Jethro", "Chris", "Xantheia"]; //Names to be displayed
+    var interval; //timeframe for rotation
+    for(var i = 0; i < numOfPeople ; i++){  //for loop to display the information for three people
+        var personIndex = Math.floor(Math.random()*names.length); //randomizing the names
+
+        var person = new Person(names[personIndex],i+1);
+        populateHTML(person.name,"r"+(i+1)+"c1"); //displays name
+        populateHTML(person.job,"r"+(i+1)+"c2");  //displays job
+
+        people.push(person); //adds
+        names.splice(personIndex,1); //subtracts
+    }
+    clearInterval(interval);
+    interval=setInterval(runUpdate,1000/30); //every 30 seconds the action will change
+    function populateHTML(data,field){      //displays the random activities
+        var id=document.getElementById(field);
+        id.innerHTML=data;
+    }
+
+    function runUpdate() {
+        people.forEach(function(element){  //function that kicks in after all instances have been used to keep the process moving
+            element.update();
+        });
+    }
+})();
--- PWA1:WK4/goal7_assign_objects/js/person_blueprint.js
+++ PWA1:WK4/goal7_assign_objects/js/person_blueprint.js
@@ -5,27 +5,27 @@
  */
 
 (function(){
-    window.Person=Person;
+    window.Person=Person; //global window object
 
-    Person.jobs = ["Lawyer","Doctor","Singer","Dancer","Teacher"];
-    Person.actions = ["Singing","Dancing","Talking","Eating","Sleeping"];
+    Person.jobs = ["Lawyer","Doctor","Singer","Dancer","Teacher"]; //all possible job options
+    Person.actions = ["Singing","Dancing","Talking","Eating","Sleeping"]; //all possible action options
     function Person(name,row){
         console.log("Person Created:",name);
 
         this.name = name;
-        this.action =Person.actions[Math.floor(Math.random()*Person.actions.length)];
-        this.job = Person.jobs[Math.floor(Math.random()*Person.jobs.length)];
-        this.row = row;
+        this.action =Person.actions[Math.floor(Math.random()*Person.actions.length)]; //random action for each person
+        this.job = Person.jobs[Math.floor(Math.random()*Person.jobs.length)];  //random job for each person
+        this.row = row; //direction of where the code goes
 
         var id = document.getElementById("r"+this.row+"c3");
-        id.innerHTML=this.action;
+        id.innerHTML=this.action; //showing the action on the page
     }
-    Person.prototype.update = function(){
+    Person.prototype.update = function(){  //pulled to the main.js to change the action
         if(Math.floor(Math.random()<01.)){
-            var i = Math.floor(Math.random()*Person.actions.length);
+            var i = Math.floor(Math.random()*Person.actions.length); //randomize the action
             this.action = Person.action[i];
-            var id = document.getElementById("r"+this.row+"c3");
+            var id = document.getElementById("r"+this.row+"c3"); //pull the information from which row and column
             id.innerHTML=this.action;
         }
     }
-}
+});
